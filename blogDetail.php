72b3fbb7050a3c7b8ec1e33f4162d853fd24fbf2
Napat Chaimetnarakul <napat.chaimet@gmail.com>
2021-08-20 15:28:57 +0700

    Add Page Blog Detail

---
+++ blogDetail.php
@@ -0,0 +1,35 @@
+<?php include('./header.php') ?>
+<?php include('./navbar.php') ?>
+<div class="blogdetail">
+    <div class="container">
+        <h2>V1 Around The World</h2>
+        <div class="blogimgheader">
+            <img src="./img/newsimg.png" width="100%" alt="">
+        </div>
+        <div class="textheader">
+            <h5>28 JULY 2020</h5>
+            <h3>MUDETEC: IN SEARCH OF THE FUTURE</h3>
+            <p>The Lamborghini Museum has been updated to become Museum of Technologies, where
+                fascinating history, the iconic models and tours of the production lines tell the story of over 50
+                years of innovation that project Lamborghini into the future. The Lamborghini Museum has been
+                updated to become Museum of Technologies, where fascinating history, the iconic models and
+                tours of the production lines tell the story of over 50 years of innovation that project Lamborghini
+                into the future.</p>
+        </div>
+        <div class="blogimgheader">
+            <img src="./img/newsimg.png" width="100%" alt="">
+        </div>
+        <div class="textheader">
+            <h3>MUDETEC: IN SEARCH OF THE FUTURE</h3>
+            <p>The Lamborghini Museum has been updated to become Museum of Technologies, where
+                fascinating history, the iconic models and tours of the production lines tell the story of over 50
+                years of innovation that project Lamborghini into the future. The Lamborghini Museum has been
+                updated to become Museum of Technologies, where fascinating history, the iconic models and
+                tours of the production lines tell the story of over 50 years of innovation that project Lamborghini
+                into the future.</p>
+        </div>
+    </div>
+</div>
+<?php include('./message.php') ?>
+<?php include('./contentfooter.php') ?>
+<?php include('./footer.php') ?>
